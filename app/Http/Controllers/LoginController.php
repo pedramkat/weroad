2451d686f94d0621f1c0753ef9d76a3aa2d86e9f
Pedram Katanchi <pedramkat@gmail.com>
2023-07-12 01:28:04 +0200

    Added swagger authentication

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,6 +10,88 @@ use Exception;
 
 class LoginController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     summary="User login",
+     *     description="Authenticate user and generate a bearer token",
+     *     tags={"Authentication"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="email",
+     *                     description="User's email",
+     *                     type="string",
+     *                     example="admin@weroad.it"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     description="User's password",
+     *                     type="string",
+     *                     example="admin"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful login",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="token",
+     *                     description="Bearer token for authentication",
+     *                     type="string",
+     *                     example="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     description="User's name",
+     *                     type="string",
+     *                     example="Jhon"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="role",
+     *                     description="User's role",
+     *                     type="string",
+     *                     example="admin"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email_verified_at",
+     *                     description="Verification time",
+     *                     type="string",
+     *                     example="2023-07-10T23:10:58.000000Z"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="success",
+     *                     description="Error status",
+     *                     type="boolean",
+     *                     example="true"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="message",
+     *                     description="Error message",
+     *                     type="string",
+     *                     example="The provided credentials are incorrect."
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function login(Request $request)
     {
         try {
--- app/Http/Controllers/V1/TravelControllerV1.php
+++ app/Http/Controllers/V1/TravelControllerV1.php
@@ -13,31 +13,61 @@ define('_BOUNDIG_BOX_LIMIT', 0.1);
 class TravelControllerV1 extends Controller
 {
     /**
-     *
      * @OA\Get(
-     *      path="/api/v1/travels",
-     *      tags={"api v1"},
-     *      @OA\Response(
-     *          response=200,
-     *          description="Returns all the public travels",
-     *       @OA\MediaType(
-     *             mediaType="application/json",
-     *             @OA\Schema(
-     *
+     *     path="/api/v1/travels",
+     *     summary="Get all travels",
+     *     description="Retrieve a list of all travels",
+     *     operationId="getAllTravels",
+     *     tags={"Api v1 - Travels"},
+     *     security={{ "sanctum": {} }},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Travel")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="error",
+     *                 type="string",
+     *                 example="Unauthorized"
      *             )
      *         )
-     *      ),
+     *     )
      * )
      *
+     * @OA\Schema(
+     *     schema="Travel",
+     *     title="Travel",
+     *     @OA\Property(
+     *         property="id",
+     *         type="integer",
+     *         description="Travel ID"
+     *     ),
+     *     @OA\Property(
+     *         property="name",
+     *         type="string",
+     *         description="Travel name"
+     *     ),
+     *     @OA\Property(
+     *         property="destination",
+     *         type="string",
+     *         description="Travel destination"
+     *     ),
+     * )
      */
+
     public function index()
     {
-        $lists = Travel::all();
-
-        $list = $lists->toArray();
+        $lists = Travel::all()->toArray();
 
         // Return
-        return response($list, 200, ['Content-type' => 'application/json']);
+        return response($lists, 200, ['Content-type' => 'application/json']);
     }
 
 }
